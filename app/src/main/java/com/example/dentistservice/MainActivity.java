1dd4b3117aba5532c4d5142610615eeb57789788
BuildTools <unconfigured@null.spigotmc.org>
2024-06-27 07:12:37 +0700

    tomatossy

--- app/src/main/java/com/example/dentistservice/MainActivity.java
+++ app/src/main/java/com/example/dentistservice/MainActivity.java
@@ -11,6 +11,7 @@ import androidx.core.view.WindowInsetsCompat;
 public class MainActivity extends AppCompatActivity {
     //ม้วนโอ้ก
     //fat rat
+    //tomatoss
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
